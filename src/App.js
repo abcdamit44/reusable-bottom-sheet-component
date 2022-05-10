9eab38fc7b339b5882fefd87cb2971237ee7903a
Amit Singh <abcdamit44@gmail.com>
2022-05-10 22:52:30 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ChildComp from './ChildComp';
 
 function App() {
+  const [show, setShow] = useState(false)
+  // const [slide, setSlide] = useState("0")
+  const showComp = () => {
+    setShow(true);
+  }
+  const slideDown = () => {
+    setShow(false);
+    // setSlide("100%");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={showComp}>Open Bottom Sheet</button>
+      <ChildComp>
+        <div onClick={slideDown} style={{
+          display: `${show ? "flex" : "none"}`,
+          position: "absolute",
+          top: `0`,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          background: "rgba(0,0,0,0.5)",
+          // display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          transition: "300ms ease-in-out linear"
+        }}>
+          <div style={{
+            width: "300px",
+            height: "400px",
+            textAlign: "center",
+            fontSize: "1.1rem",
+            background: "#fff",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+            Hello I am Child
+          </div>
+        </div>
+      </ChildComp>
     </div>
   );
 }
---
+++ src/ChildComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChildComp = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
+export default ChildComp
